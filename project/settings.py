c2a550023325c1d8baf6a5259ccf08859005cc2b
Satish <kandepallysatish123@gmail.com>
2023-05-08 20:28:15 +0530

    postgre db name change

--- project/settings.py
+++ project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'appsDB',
+        'NAME': 'appsdb',
         'USER': 'apps_admin',
         'PASSWORD': 'seadeskapps123',
         'HOST': 'localhost',
