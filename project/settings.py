79059b43a0bf85092172e94ecc779ee72372fb14
Satish <kandepallysatish123@gmail.com>
2023-05-08 21:13:16 +0530

    media related html change

--- project/settings.py
+++ project/settings.py
@@ -143,6 +143,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "/root/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "/root/static")
